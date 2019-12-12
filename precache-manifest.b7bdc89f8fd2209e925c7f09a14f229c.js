a7cd7cc5d2836a18a72c2c2655f73d9b2b0b7a0d
Slava <Mr-93@mail.ru>
2019-12-12 17:14:08 +0300

    Updates

---
+++ precache-manifest.b7bdc89f8fd2209e925c7f09a14f229c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41b34798f5d7b709c951902b0c77f5e",
+    "url": "/scrumlogic_test_app/index.html"
+  },
+  {
+    "revision": "133f4d10905d1dbce590",
+    "url": "/scrumlogic_test_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fd5f7ead33e8ce5688ec",
+    "url": "/scrumlogic_test_app/static/js/2.22e6b741.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/scrumlogic_test_app/static/js/2.22e6b741.chunk.js.LICENSE"
+  },
+  {
+    "revision": "133f4d10905d1dbce590",
+    "url": "/scrumlogic_test_app/static/js/main.17bba2ce.chunk.js"
+  },
+  {
+    "revision": "e0b408d021b9f495fdba",
+    "url": "/scrumlogic_test_app/static/js/runtime-main.a763d682.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scrumlogic_test_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
